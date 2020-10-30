c7e1eae1c19f75f4c2a4e8828a351b773fbdf836
ManhCuong <cuongtop4598@gmail.com>
2020-10-30 07:45:02 +0700

    supplement component for form AddHangHoa

--- QuanLySieuThi/BanHangSieuTHi/frmAddHangHoa.cs
+++ QuanLySieuThi/BanHangSieuTHi/frmAddHangHoa.cs
@@ -17,6 +17,29 @@ namespace BanHangSieuTHi.Add
         public frmHangHoa(ArrayList arHH)
         {
             InitializeComponent();
+            if (arHH.Count == 0)
+            {
+                sqlQuery truyVanDL = new sqlQuery();
+                DataTable dt = truyVanDL.LayDuLieu("select * from LOAIHANG");
+                ArrayList arLH = new ArrayList();
+                foreach (DataRow row in dt.Rows)
+                {
+                    arLH.Add(row["DienGiai"]);
+                }
+                Loai.DataSource = arLH;
+
+                DataTable dtHH = truyVanDL.LayDuLieu("select * from HANGHOA order by MaHang desc");
+                string MaHang = "";
+                foreach (DataRow row in dtHH.Rows)
+                {
+                    MaHang = row["MaHang"].ToString();
+                    int k = MaHang.IndexOf("h");
+                    int t = int.Parse(MaHang.Substring(k + 1)) + 1;
+                    Ma.Text = "h" + t;
+                    break;
+                }
+
+            }
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -31,6 +54,20 @@ namespace BanHangSieuTHi.Add
 
         private void button1_Click(object sender, EventArgs e)
         {
+            sqlQuery truyVan = new sqlQuery();
+            DataTable dt = truyVan.LayDuLieu("select * from LOAIHANG where DienGiai = N'" + Loai.Text + "'");
+            Object MaLoai = "";
+            foreach (DataRow row in dt.Rows)
+            {
+                MaLoai = row["MaLoai"];
+            }
+
+            string query = "insert into HANGHOA values('" + Ma.Text + "','" + Ten.Text + "','" + DonVi.Text + "','" + TenNhaSX.Text + "'," + GiaDeNghi.Text + ",'" + MaLoai + "'," + SoLuong.Text + ")";
+
+            truyVan.ExecuteNoneQuery(query);
+
+
+            Close();
         }
     }
 }
